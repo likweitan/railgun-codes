490a6acb8c673998826ed93400a8c43fee303fae
likweitan <likweitan@gmail.com>
2021-02-25 19:39:59 +0800

    Coming Soon

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import Greet from "./components/Greet";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
+        <p>Coming Soon</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://blog.railgun.codes/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          railgun.codes
         </a>
       </header>
     </div>
---
+++ src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+//function Greet() {
+//  return <h1>hi</h1>;
+//}
+
+const Greet = () => <h1>hi noob</h1>;
+
+export default Greet;
